fac69e7dcfa880123b949c00be8a6b4f9fcd3fe0
osama-hasan-khan <osamahasankhan098@gmail.com>
2024-02-17 08:20:35 +0530

    product detaile page

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,12 +1,19 @@
-import { Image, StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { Image, Pressable, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
 import { Stack, useLocalSearchParams } from "expo-router";
 import products from "@assets/data/products";
+import Button from "@/components/Button";
+
+const sizes = ["S", "M", "L", "Xl"];
 
 const ProductDetailsScreen = () => {
   const { id } = useLocalSearchParams();
 
+  const [selectedSize, setSelectedSize] = useState("M");
+
   const product = products.find((p) => p.id.toString() === id);
+
+  const addToCart = () => {};
 
   if (!product) {
     return <Text>Product not found</Text>;
@@ -16,7 +23,40 @@ const ProductDetailsScreen = () => {
     <View style={styles.container}>
       <Stack.Screen options={{ title: product.name }} />
       <Image source={{ uri: product?.image }} style={styles.image} />
+
+      <Text>Select Size</Text>
+      <View style={styles.sizes}>
+        {sizes.map((size) => {
+          return (
+            <Pressable
+              onPress={() => {
+                setSelectedSize(size);
+              }}
+              key={size}
+              style={[
+                styles.size,
+                {
+                  backgroundColor:
+                    selectedSize === size ? "gainsboro" : "white",
+                },
+              ]}
+            >
+              <Text
+                style={[
+                  styles.sizeText,
+                  { color: selectedSize === size ? "black" : "gray" },
+                ]}
+              >
+                {size}
+              </Text>
+            </Pressable>
+          );
+        })}
+      </View>
+
       <Text style={styles.price}>${product?.price}</Text>
+
+      <Button text="Add to cart" onPress={addToCart} />
     </View>
   );
 };
@@ -38,5 +78,25 @@ const styles = StyleSheet.create({
   price: {
     fontSize: 18,
     fontWeight: "bold",
+    marginTop: "auto",
+  },
+
+  sizes: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+
+  size: {
+    backgroundColor: "gainsboro",
+    width: 50,
+    aspectRatio: 1,
+    borderRadius: 25,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  sizeText: {
+    fontSize: 20,
+    fontWeight: "500",
   },
 });
